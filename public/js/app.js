e52f01347eb8dc6207a2085a1c03ba7c1a2044c6
JoilsonJunior <joilson320@hotmail.com>
2020-06-27 14:16:27 -0300

    Atualzar projeto scroll

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,14 @@
+$('nav a').click(function(e){
+    e.preventDefault();
+    
+    var id = $(this).attr('href'),
+       targetOffset = $(id).offset().top,
+       menuHeight = $('nav').innerHeight();
+       
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+});
+
 
 
