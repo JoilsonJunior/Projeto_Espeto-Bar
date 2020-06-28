c0f31adac23a9e22aa80e48f62fffdc7d552583f
JoilsonJunior <joilson320@hotmail.com>
2020-06-28 13:48:36 -0300

    Projeto Atualizado

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,1 @@
-// Identificar o clique no menu
-// Verificar o item que foi clicado e fazer referência com o alvo
-// Verificar a distância entre o alvo e o topo
-// Animar o scroll até o alvo
-
-const menuItems = document.querySelectorAll('.nav-links a[href^="#"]');
-
-menuItems.forEach(item => {
-    item.addEventListener('click' , scrollToIdOnClick);
-})
-
-function scrollToIdOnClick() {
-    console.log('clicou');
-}
-
 
